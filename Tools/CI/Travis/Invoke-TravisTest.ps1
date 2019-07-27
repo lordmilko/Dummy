0c28d72eac54365a221b67b87e96cd1fb2a4c820
lordmilko <lordmilko@users.noreply.github.com>
2019-07-27 21:20:06 +1000

    a

--- Tools/CI/Travis/Invoke-TravisTest.ps1
+++ Tools/CI/Travis/Invoke-TravisTest.ps1
@@ -2,7 +2,12 @@ function Invoke-TravisTest
 {
     Write-LogHeader "Executing tests"
 
-    Invoke-CIPowerShellTest $env:TRAVIS_BUILD_DIR (@{ExcludeTag = "Build"}) -IsCore:$true | Out-Null
+    $result = Invoke-CIPowerShellTest $env:TRAVIS_BUILD_DIR (@{ExcludeTag = "Build"}) -IsCore:$true
+
+    if($result.FailedCount -gt 0)
+    {
+        throw "$($result.FailedCount) Pester tests failed"
+    }
 
     $csharpArgs = @(
         "--filter"
