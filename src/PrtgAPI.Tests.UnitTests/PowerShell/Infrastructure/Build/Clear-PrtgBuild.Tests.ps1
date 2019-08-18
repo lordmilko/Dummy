733c02f51c7c078f173d37f68c29ca5b426b0cba
lordmilko <lordmilko@users.noreply.github.com>
2019-08-18 23:18:20 +1000

    a

--- src/PrtgAPI.Tests.UnitTests/PowerShell/Infrastructure/Build/Clear-PrtgBuild.Tests.ps1
+++ src/PrtgAPI.Tests.UnitTests/PowerShell/Infrastructure/Build/Clear-PrtgBuild.Tests.ps1
@@ -2,11 +2,7 @@
 
 Describe "Clear-PrtgBuild" -Tag @("PowerShell", "Build") {
 
-    $solutionRoot = Get-SolutionRoot
-
     It "clears core" {
-
-        Mock-InstallDotnet -Windows
 
         Mock Invoke-CIProcess {
             Write-Host "called"
